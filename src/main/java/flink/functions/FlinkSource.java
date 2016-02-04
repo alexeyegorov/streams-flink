907e226c16818cd06fd55ba402bbfdc6aaf7909c
Alexey Egorov <aegorov87@gmail.com>
2016-02-04 16:34:03 +0100

    remove unused import (now uses parallel source function)

--- src/main/java/flink/functions/FlinkSource.java
+++ src/main/java/flink/functions/FlinkSource.java
@@ -1,7 +1,6 @@
 package flink.functions;
 
 import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
